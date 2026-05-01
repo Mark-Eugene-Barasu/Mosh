9017c8fe5960e55f8a851cd03e084087b3db052d
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2026-05-01 09:50:40 +0200

    Using the default argument

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def increment(number, by):
+# default arguments
+
+def increment(number, by=1):
     return number + by
 
-result = increment(5, 3)
-print(result)  # Output: 8
-
-print(increment(10, 2))  # Output: 12
-
-print(increment(-1, by=5))  # Output: 4 
+print(increment(5))
