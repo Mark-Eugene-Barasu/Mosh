5d5837300ec251d3090658308d069d642921b62f
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2026-05-01 08:28:38 +0200

    short-circuit evaluation

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-# local operators
+# short-circuit evaluation
 high_income = False
 good_credit = True
 student = False
 
 
-if (high_income or good_credit) and not student:
-    print("Eligible for loan")
-else:
-    print("Not eligible for loan") 
+if high_income or good_credit and not student:
+    print("Eligible for loan") 
 
