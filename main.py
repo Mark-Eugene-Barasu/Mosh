c73b2f7f0dd7eb0568f66c65939bee4de86dd0c8
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2026-05-01 09:10:27 +0200

    while loop

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-# iteration
-print(type(5))
-print(type(range(5)))
-
-
-# Iterable
-for x in [1, 2, 3, 4, 5]:
-    print(x)
-
-shopping_cart = ["milk", "bread", "eggs", "cheese"]
-for item in shopping_cart:
-    print(item)
+command = ""
+while command.lower().strip() != "exit":
+    command = input(">")
+    print("ECHO", command)
