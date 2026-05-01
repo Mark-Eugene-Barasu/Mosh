cecf1669ccab0ad87f0c1256a06c5746b0438cc0
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2026-05-01 09:48:21 +0200

    keyword argument

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
-def greet(name):
-    return f"Hello, {name}!"
+def increment(number, by):
+    return number + by
 
-# we use a return to return a value from the function, instead of printing it directly. This allows us to use the returned value in other parts of our code, or to store it in a variable for later use.
+result = increment(5, 3)
+print(result)  # Output: 8
 
-def get_greeting(name):
-    return f"Hello, {name}!"
+print(increment(10, 2))  # Output: 12
 
-message = get_greeting("Eugene")
-print(message)  # Output: Hello, Eugene!
-# In this example, the get_greeting function returns a greeting message, which we then store in the variable message and print it. This approach is more flexible than printing the message directly within the function, as it allows us to use the returned value in different contexts.
-
-file = open("file.txt", "w")
-file.write(message)
-file.close()
+print(increment(-1, by=5))  # Output: 4 
