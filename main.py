815a98e67a46257a2699ed56299e075070e3beb9
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2026-05-01 07:24:28 +0200

    data types

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-# PEP8 (Python Enhancement Proposal 8) - Style Guide for Python Code
+# Fundamental of python
 
-X = 1
+# 1. Variable and Data Types
+student_count = 1000 # Integer
+rating = 4.99 # Float
+is_published = True # Boolean
+course_name = "Python Programming" # String
+print(student_count)
+print(rating)
+print(is_published)
+print(course_name)
+
+# 2. Control Flow 
