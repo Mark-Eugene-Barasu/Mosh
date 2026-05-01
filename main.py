83b24534c23de5cb3f5247d9f59a10d6fd860801
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2026-05-01 07:30:13 +0200

    using the len() method and using indices to get specific element of the string

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
-# Fundamental of python
-
-# 1. Variable and Data Types
-student_count = 1000 # Integer
-rating = 4.99 # Float
-is_published = True # Boolean
-course_name = "Python Programming" # String
-print(student_count)
-print(rating)
-print(is_published)
-print(course_name)
-
-# 2. Control Flow 
+course = "Python Programming"
+print(len(course))
+print(course[0])
+print(course[-1])
+print(course[0:3])
+print(course[0:])
+print(course[:3])
+print(course[:])   
