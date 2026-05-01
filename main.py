712db3d150810a2624394318a4f3aa915162fb26
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2026-05-01 07:36:15 +0200

    escaping character

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-course = "Python Programming"
-print(len(course))
-print(course[0])
-print(course[-1])
-print(course[0:3])
-print(course[0:])
-print(course[:3])
-print(course[:])   
+# \"
+# \'
+# \\
+# \n
+# \t
+
+course = "Python \t\"Programming\""
+print(course)
