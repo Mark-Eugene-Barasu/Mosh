50af05409c2204f7b8fde4faddedee50c23332da
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2026-05-01 08:54:10 +0200

    using if statement in for loop

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-for number in range(1, 10, 2):
-    print("Attempt", number, (number + 1) * ".")
+successful = True
+for number in range(3):
+    print("Attempt")
+    if successful:
+        print("Successful")
+        break
+else:
+    print("Attempted 3 times and failed")
